f11467e87a0427fa024d343a25357be5b676a5f2
azhar <contact.ajharislam@gmail.com>
2021-09-20 18:45:34 +0600

    change image path

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,67 +13,67 @@ const products=[
 	id:"01",
 	description:"iPhone-12",
 	price:"BTD-180000",
-	src:"/img/product.png"
+	src:"img/product.png"
 },
 { 
 	id:"02",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"03",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"04",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"05",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"06",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"07",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"08",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"09",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:" img/product.png",
 },
 {
 	id:"10",
 	description:"iPhone-11 ",
 	price:"BDT-160000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 {
 	id:"12",
 	description:"iPhone-12 ",
 	price:"BDT-180000",
-	src:"/img/product.png",
+	src:"img/product.png",
 },
 ]
 const productLink="http://localhost:3000/hulusthul/Shop"
